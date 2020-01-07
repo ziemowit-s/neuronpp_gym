f1397f352bc26a87955fe0f3b424c1aef188d5e5
Ziemowit S <ziemowit.slawinski@gmail.com>
2020-01-08 00:39:29 +0100

    init

---
+++ pong_gym.py
@@ -0,0 +1,18 @@
+import time
+
+import gym
+
+debug = True
+
+with gym.make('Pong-v0') as env:
+    env.reset()
+
+    print('action_space', env.action_space)
+    print('action_space', env.observation_space)
+    print('reward_range', env.reward_range)
+
+    for _ in range(1000):
+        env.render(mode="human")
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        time.sleep(0.01)
