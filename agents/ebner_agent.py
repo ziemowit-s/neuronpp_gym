4711165558ed17bb2406ee08a1406a2275783183
Ziemowit S <ziemowit.slawinski@gmail.com>
2020-01-19 11:09:43 +0100

    updated agent to return actions as time of spikes in ms

--- agents/ebner_agent.py
+++ agents/ebner_agent.py
@@ -1,57 +1,73 @@
 import numpy as np
+from neuron import h
 
-from neuronpp.utils.record import Record
 from neuronpp.cells.ebner2019_ach_da_cell import Ebner2019AChDACell
-
-from agents.basic_agent import BasicAgent
+from neuronpp.utils.run_sim import RunSim
 
 WEIGHT = 0.0035
 
 
-class EbnerAgent(BasicAgent):
+class EbnerAgent:
     def __init__(self, input_size, max_hz, stepsize=20, finalize_step=5, warmup=200):
-        super().__init__(input_size, max_hz, stepsize, finalize_step, warmup)
+        self.stepsize = stepsize
+        self.max_stim_num = 1000 / stepsize
+        self.max_hz = max_hz
+        self.finalize_step = finalize_step
+
+        self.input_synapses = self._build_cells(input_size)
+        self.time_vec = h.Vector().record(h._ref_t)
+
+        # init and warmup
+        self.sim = RunSim(init_v=-70, warmup=warmup)
+        print("Agent setup done")
 
     def step(self, observation=None, reward=None):
+        """
+
+        :param observation:
+        :param reward:
+        :return: 
+            Return actions as numpy array of time of spikes in ms.
+        """
         spiked_pixels = 0
         # Observe
         if observation is not None or reward is not None:
-            for input_value, synapse in zip(observation, self.inputs):
+            for input_value, synapse in zip(observation, self.input_synapses):
                 is_spiked = self._make_stim(input_value=input_value, synapse=synapse)
                 spiked_pixels += is_spiked
 
         print('pixel which spiks:', spiked_pixels, 'obs>0:', np.sum(observation > 0))
 
         # Run
-        self.sim.run(self.sim_step + self.finalize_step)
+        self.sim.run(self.stepsize + self.finalize_step)
 
-        # Read actions
-        output = self.output_rec.recs['v'][0][1].as_numpy()
-        return output
+        # Return actions as time of spikes in ms
+        time_of_spikes = self.cell.get_spikes()
+        return time_of_spikes
 
-    def _prepare_cell(self, input_size, delay=1):
+    def _build_cells(self, input_size, delay=1):
         # Prepare cell
-        self.input_cell = Ebner2019AChDACell("input_cell")
-        self.input_cell.make_sec("soma", diam=10, l=10, nseg=10)
-        self.input_cell.make_sec("dend", diam=4, l=50, nseg=10)
-        self.input_cell.connect_secs(source="dend", target="soma", source_loc=0, target_loc=1)
+        self.cell = Ebner2019AChDACell("input_cell")
+        self.cell.make_sec("soma", diam=10, l=10, nseg=10)
+        self.cell.make_sec("dend", diam=4, l=50, nseg=10)
+        self.cell.connect_secs(source="dend", target="soma", source_loc=0, target_loc=1)
 
         # make synapses with spines
-        syn_4p, heads = self.input_cell.make_spine_with_synapse(source=None, number=100, weight=WEIGHT, mod_name="Syn4PAChDa",
-                                                                delay=delay, **self.input_cell.params_4p_syn)
-        syn_ach = self.input_cell.make_sypanses(source=None, weight=WEIGHT, mod_name="SynACh", sec=heads, delay=delay,
-                                                **self.input_cell.params_ach)
-        syn_da = self.input_cell.make_sypanses(source=None, weight=WEIGHT, mod_name="SynDa", sec=heads, delay=delay,
-                                               **self.input_cell.params_da)
-        self.input_cell.set_synaptic_pointers(syn_4p, syn_ach, syn_da)
+        syn_4p, heads = self.cell.make_spine_with_synapse(source=None, number=input_size, weight=WEIGHT, mod_name="Syn4PAChDa",
+                                                          delay=delay, **self.cell.params_4p_syn)
+
+        syn_ach = self.cell.make_sypanses(source=None, weight=WEIGHT, mod_name="SynACh", sec=heads, delay=delay, **self.cell.params_ach)
+        syn_da = self.cell.make_sypanses(source=None, weight=WEIGHT, mod_name="SynDa", sec=heads, delay=delay, **self.cell.params_da)
+        self.cell.set_synaptic_pointers(syn_4p, syn_ach, syn_da)
 
         # Add mechanisms
-        self.input_cell.make_soma_mechanisms()
-        self.input_cell.make_apical_mechanisms(sections='dend head neck')
+        self.cell.make_soma_mechanisms()
+        self.cell.make_apical_mechanisms(sections='dend head neck')
 
-        self.output_rec = Record(self.input_cell.filter_secs(name="soma"), locs=0.5, variables="v")
+        self.cell.make_spike_detector(sec="soma", loc=0.5)
 
-        return list(zip(syn_4p, syn_ach, syn_da))
+        input_synapses = list(zip(syn_4p, syn_ach, syn_da))
+        return input_synapses
 
     def _make_stim(self, input_value, synapse):
         """
@@ -72,3 +88,8 @@ class EbnerAgent(BasicAgent):
             syn4p.make_event(next_event)
             next_event += interval
         return stim_num > 0
+
+    def get_single_stim(self, input_value):
+        stim_num = int(round((input_value * self.max_hz) / self.max_stim_num))
+        stim_int = self.stepsize / stim_num if stim_num > 0 else 0
+        return stim_num, stim_int
--- pong_gym_run.py
+++ pong_gym_run.py
@@ -1,7 +1,3 @@
-import time
-import numpy as np
-from neuron import h
-
 import matplotlib.pyplot as plt
 
 from agents.ebner_agent import EbnerAgent
@@ -23,9 +19,6 @@ def plot_spikes(v, agent, title=None, ax=None):
 
 
 if __name__ == '__main__':
-    fig, axs = plt.subplots(1, clear=True)
-    fig.show()
-    plt.show()
 
     env, input_size = get_env('Pong-v0', ratio=SCREEN_RATIO)
     agent = EbnerAgent(input_size=input_size, max_hz=300, stepsize=100, finalize_step=5, warmup=200)
@@ -35,16 +28,17 @@ if __name__ == '__main__':
     for i in range(ENV_STEP):
 
         env.render()
+
         obs, reward, done, info = env.step(env.action_space.sample())
         obs = prepare_pong_observation(obs, ratio=SCREEN_RATIO, show=False)
+
         if done or reward != 0:
             print('reward:', reward, "done:", done)
             env.reset()
 
-        output = agent.step(observation=obs, reward=reward)
-        print("output max:", np.max(output))
-        plot_spikes(output, agent=agent, ax=axs)
+        time_of_spikes = agent.step(observation=obs, reward=reward)
 
-    agent.output_rec.plot()
-    plt.show()
+        agent.cell.plot_spikes()
+        plt.pause(1.0)
+        plt.close()
     env.close()
