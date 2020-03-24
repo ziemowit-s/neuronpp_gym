fixed 2d encoding; added average accuracy form MNIST with FIFO=50 elements

agents/agent.py
@@ -33,11 +33,13 @@ class Agent:
 
         self.sim = None
         self.warmup = None
+
         self.input_size = None
         self.input_shape = None
         self.input_cells = None
-        self.output_cells = None
         self.input_cell_num = None
+
+        self.output_cells = None
 
         self.x_kernel = None
         self.y_kernel = None
@@ -81,8 +83,10 @@ class Agent:
         self.x_kernel = x_kernel
         self.y_kernel = y_kernel
 
-        self.input_shape = input_shape
-        self.input_size = np.prod(input_shape)
+        padded_obs = self.pad_2d_observation(np.zeros(input_shape))
+
+        self.input_shape = padded_obs.shape
+        self.input_size = padded_obs.size
         self.input_cell_num = self.x_kernel_num * self.y_kernel_num
 
         self._build()
@@ -167,8 +171,6 @@ class Agent:
                               "however input_cells were not defined.")
 
         # Make observation
-        if self.input_size != observation.size:
-            raise RuntimeError("Observation must be of same size as self.input_size, which is a product of input_shape.")
         if observation.ndim == 1:
             self._make_1d_observation(observation)
         elif observation.ndim == 2:
@@ -221,7 +223,7 @@ class Agent:
         return result
 
     def pad_2d_observation(self, obs):
-        if not self._agent_builded:
+        if self.x_kernel is None or self.y_kernel is None:
             raise RuntimeError("Before calling pad_observation() you need to build the Agent by calling build() function first.")
         return np.pad(obs, (self.x_kernel.padding, self.y_kernel.padding), 'constant', constant_values=(0, 0))
 
@@ -288,6 +290,8 @@ class Agent:
             list of names of stimulated cells in the stimulation order
         """
         obs = self.pad_2d_observation(obs)
+        if self.input_size != obs.size:
+            raise RuntimeError("Observation must be of same size as self.input_size, which is a product of input_shape.")
 
         cell_i = 0
         for y in range(0, self.input_shape[0], self.y_kernel.stride):
@@ -308,6 +312,9 @@ class Agent:
         1 dim array of numbers
         :return:
         """
+        if self.input_size != obs.size:
+            raise RuntimeError("Observation must be of same size as self.input_size, which is a product of input_shape.")
+
         input_syns = [s for c in self.input_cells for s in c.syns]
         self._make_single_observation(observation=obs, syns=input_syns)
 

mnist_run.py
@@ -1,7 +1,8 @@
+import queue
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from agents.agent import Kernel
 from neuronpp.utils.network_status_graph import NetworkStatusGraph
@@ -87,6 +88,7 @@ network_graph.plot()
 index = 0
 reward = None
 agent_compute_time = 0
+fifo = queue.Queue(50)
 
 while True:
     # Get current mnist data
@@ -109,9 +111,11 @@ while True:
 
     # Make reward
     if predicted == y:
+        fifo.put(1)
         reward = 1
         print("i:", index, "reward recognized", y)
     else:
+        fifo.put(0)
         reward = -1
 
     # Update graphs
@@ -121,7 +125,8 @@ while True:
 
     # Update visualizations
     imshow_obj.set_data(agent.pad_2d_observation(obs))
-    ax.set_title('Predicted: %s True: %s' % (predicted, y))
+    avg_accuracy = round(np.average(list(fifo.queue)), 2)
+    ax.set_title('Predicted: %s True: %s, AVG_ACC: %s' % (predicted, y, avg_accuracy))
     plt.draw()
     plt.pause(1e-9)
 