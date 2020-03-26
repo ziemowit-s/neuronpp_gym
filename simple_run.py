2b296b54f8a8c4b1464ab912d111d930c004a453
Ziemowit S <ziemowit.slawinski@gmail.com>
2020-03-26 14:01:42 +0100

    added amplitude for Ebner's synapse ACh and Da learning; ACh and Da are multiplied by the current w of the Ebner synapse; changed simple_run.py to show ACh and Da synapses params; changed name from naive_run.py to simple_run.py

--- simple_run.py
+++ simple_run.py
@@ -2,10 +2,10 @@ import queue
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from neuronpp.utils.record import Record
 
 from agents.ebner_agent import EbnerAgent
 from agents.sigma3_agent import Sigma3Agent
-from neuronpp.utils.spikes_heatmap_graph import SpikesHeatmapGraph
 
 
 def make_image():
@@ -22,28 +22,34 @@ MAX_AVG_SIZE = 50  # Max size of average window to count accuracy
 # Create Agent
 agent = EbnerAgent(output_cell_num=2, input_max_hz=50, netcon_weight=0.01, default_stepsize=AGENT_STEPSIZE)
 agent.build(input_shape=2)
+
+# Get output synapses
+syns0 = [s for s in agent.output_cells[0].syns if "synach" not in s.name.lower() and "synda" not in s.name.lower()]
+syns1 = [s for s in agent.output_cells[1].syns if "synach" not in s.name.lower() and "synda" not in s.name.lower()]
+# Record output synapses
+syn_rec = Record(elements=syns0 + syns1, variables="ACh Da")
+
+# Init Agent
 agent.init(init_v=-70, warmup=100, dt=0.1)
 print("Input neurons:", agent.input_cell_num)
 
 # Show and update mnist image
 imshow_obj, ax = make_image()
 
-# Create heatmap graph for input cells
-hitmap_graph = SpikesHeatmapGraph(name="Input Cells", cells=agent.input_cells)
-
+# Run training
 index = 0
 reward = None
 agent_compute_time = 0
 avg_acc_fifo = queue.Queue(maxsize=MAX_AVG_SIZE)
 while True:
-    # Get current mnist data
+    # get current obs and y
     obs = np.random.randint(low=0, high=2, size=2)
     if obs[0] == 1:
         obs[1] = 0
-        y = 1
+        y = 0
     else:
         obs[1] = 1
-        y = 0
+        y = 1
 
     # Write time before agent step
     current_time_relative = (time.time() - agent_compute_time)
@@ -56,16 +62,13 @@ while True:
     predicted = -1
     outputs = agent.step(observation=obs, output_type="rate", sort_func=lambda x: -x.value, poisson=True)
 
-    print('output:', " / ".join(["%s:%s" % (o.index, o.value) for o in outputs]))
+    # Print Agent's output and output cell synapses
+    syns0_w = [round(s.hoc.w, 4) for s in syns0]
+    syns1_w = [round(s.hoc.w, 4) for s in syns1]
+    print('output:', ",".join(["%s:%s" % (o.index, o.value) for o in outputs]), "// cell_0:", syns0_w, "cell_1:", syns1_w)
 
-    syns0 = [round(s.hoc.w, 2) for s in agent.output_cells[0].syns if "synach" not in s.name.lower() and "synda" not in s.name.lower()]
-    syns1 = [round(s.hoc.w, 2) for s in agent.output_cells[1].syns if "synach" not in s.name.lower() and "synda" not in s.name.lower()]
-
-    print("c1:", syns0, "c2:", syns1)
-
-    if (outputs[0].value - outputs[1].value) >= EPSILON_OUTPUT:
+    if outputs[0].value != -1 and (outputs[1].value == -1 or (outputs[0].value - outputs[1].value) >= EPSILON_OUTPUT):
         predicted = outputs[0].index
-        print("answer:", predicted)
 
     # Make reward
     if avg_acc_fifo.qsize() == MAX_AVG_SIZE:
@@ -73,19 +76,20 @@ while True:
     if predicted == y:
         avg_acc_fifo.put(1)
         reward = 1
-        print("i:", index, "value:", y)
+        print("answer:", predicted, "OK!")
     else:
         avg_acc_fifo.put(0)
         reward = -1
-
-    # Update hitmap
-    hitmap_graph.plot()
+        print("answer:", predicted)
 
     # Update image
     obs = obs.reshape([2, 1])
     imshow_obj.set_data(obs)
     avg_accuracy = round(np.average(list(avg_acc_fifo.queue)), 2)
     ax.set_title('Predicted: %s True: %s, AVG_ACC: %s' % (predicted, y, avg_accuracy))
+
+    # Update synapse plot
+    syn_rec.plot(animate=True, position="merge")
     plt.draw()
     plt.pause(1e-9)
 
